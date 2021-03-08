6f236ef954be7f5aa6cde64b43d30c038837a1d7
Leonardo Manzano Salazar <leonardo.manzano@outlook.es>
2021-03-07 22:41:24 -0600

    primer commit

---
+++ app/src/main/java/com/leonardo/gitandroidprobando3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.leonardo.gitandroidprobando3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
